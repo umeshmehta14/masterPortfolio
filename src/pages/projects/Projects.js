794580e5d1dc664d84f510b7ea3d112903120c9f
umeshmehta14 <ishaanmehta782@gmail.com>
2023-07-07 23:16:41 +0530

    add comments

--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -2,15 +2,15 @@ import React, { Component } from "react";
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import GithubRepoCard from "../../components/githubRepoCard/GithubRepoCard";
-import PublicationCard from "../../components/publicationsCard/PublicationCard";
-import Button from "../../components/button/Button";
+// import PublicationCard from "../../components/publicationsCard/PublicationCard";
+// import Button from "../../components/button/Button";
 import TopButton from "../../components/topButton/TopButton";
 import { Fade } from "react-reveal";
 import {
-  greeting,
+  // greeting,
   projectsHeader,
-  publicationsHeader,
-  publications,
+  // publicationsHeader,
+  // publications,
 } from "../../portfolio.js";
 import ProjectsData from "../../shared/opensource/projects.json";
 import "./Projects.css";
